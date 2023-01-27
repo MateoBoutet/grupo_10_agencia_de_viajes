fa393a9fe975122e55d23fa2df661e9464437d7f
Georgina Pena <georgina@MacBook-Pro-de-Georgina.local>
2023-01-27 16:55:26 -0300

    se aplica mobile first a register y se adapta a las validaciones

--- src/routes/register.js
+++ src/routes/register.js
@@ -5,13 +5,14 @@ let registerController = require ('../controllers/registerController');
 const {body} = require('express-validator');
 
 const validations = [
-    body('email').notEmpty().bail().withMessage('Este campo es obligatorio').isEmail().withMessage('El formato de Email debe ser válido'),
+    body('email').notEmpty().withMessage('Este campo es obligatorio').bail().isEmail().withMessage('El formato de Email debe ser válido'),
     body('name').notEmpty().withMessage('Este campo es obligatorio'),
     body('surname').notEmpty().withMessage('Este campo es obligatorio'),
     body('birthdate').notEmpty().withMessage('Este campo es obligatorio'),
-    body('phone').notEmpty().withMessage('Este campo es obligatorio'),
+    body('phone').notEmpty().withMessage('Este campo es obligatorio').bail().isInt().withMessage('debe ingresar sólo números'),
     body('password').notEmpty().withMessage('Este campo es obligatorio'),
     body('confpassword').notEmpty().withMessage('Este campo es obligatorio'),
+    body('dni').notEmpty().withMessage('Este campo es obligatorio').bail().isInt().withMessage('debe ingresar sólo números'),
 ]
 
 router.get('/',registerController.register);
